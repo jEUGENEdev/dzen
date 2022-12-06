5af78889c2dc0716e753e23a9b22fdb1c53ff2c0
Eugene <genyamateyuk@gmail.com>
2022-12-07 01:01:58 +0700

    added injection for activity

--- app/src/main/java/com/vkontakte/dzen/MainActivity.java
+++ app/src/main/java/com/vkontakte/dzen/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.vkontakte.dzen.di.ActivityInjection;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityInjection.getInstance().add(getClass(), this);
 
         startActivity(new Intent(this, SelectionActivity.class));
     }
